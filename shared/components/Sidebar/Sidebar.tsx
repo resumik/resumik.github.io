7d9e81585b1bff0094d66d95c28158272b50515d
Bartosz Konikiewicz <izdwuut@gmail.com>
2021-06-03 06:25:01 +0200

    Add /design page

--- shared/components/Sidebar/Sidebar.tsx
+++ shared/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import useSidebarStyles from './Sidebar.styles'
-import _sidebar from '../../../content/design/_sidebar.md'
+import MDXComponent from '../../../content/design/_sidebar.md'
 import useEnv from '../../hooks/useEnv'
 
 export const Sidebar = () => {
   const styles = useSidebarStyles()
-  const { html } = _sidebar
-    const env = useEnv()
+  const env = useEnv()
   return (
     <div className={styles.root}>
       <div className={styles.appName}>{env.appName}</div>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div>
+        <MDXComponent />
+      </div>
     </div>
   )
 }
