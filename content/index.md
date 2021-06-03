e93aa09e8b9c27c0fc9c4f381b14bda7968ca5d7
Bartosz Konikiewicz <izdwuut@gmail.com>
2021-06-03 06:16:13 +0200

    Use MDX

--- content/index.md
+++ content/index.md
@@ -1,5 +1,1 @@
----
-designDocumentUrl: https://resumik.github.io/resumik
----
-
 The soon-to-be state-of-the-art open-source hyphens-overload resume generator.
--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-module.exports = {
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      use: 'frontmatter-markdown-loader',
-    })
-    return cfg
-  },
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx|md$/,
+})
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   env: {
     APP_NAME: 'Resumik',
     GITHUB_REPO: 'https://github.com/resumik/resumik',
   },
-}
+})
